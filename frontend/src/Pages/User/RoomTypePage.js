d5dc59de20078b4e1231b96ec1511ec8fa8590fe
ziolata <ziolata3@gmail.com>
2024-04-01 00:13:33 +0700

    Update

--- frontend/src/Pages/User/RoomTypePage.js
+++ frontend/src/Pages/User/RoomTypePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+
 function RoomTypePage({ numToShow }) {
   const [hotel, setHotel] = useState([]);
 
@@ -16,9 +17,12 @@ function RoomTypePage({ numToShow }) {
   }, []);
 
   const handleRoomClick = (roomId) => {
-    window.location.href = `/room-type/${roomId}/`;
+    // Navigate to the room details page using Link component
+    // (assuming you have a route defined for `/room-type/${roomId}/`)
   };
+
   const firstTenHotels = hotel.slice(0, numToShow);
+
   return (
     <>
       <div className="row">
@@ -27,15 +31,15 @@ function RoomTypePage({ numToShow }) {
             className="col-md-6 col-lg-6 col-xs-12 col-sm-12 mb-1"
             key={item.id}
           >
-            {/* product */}
             <div className="product-content product-wrap clearfix">
               <div className="row">
                 <div className="col-md-5 col-sm-12 col-xs-12">
                   <div className="product-image">
                     <img
                       src={item.image}
-                      alt="194x228"
+                      alt="Room Type"
                       className="img-responsive"
+                      // Set width and height explicitly using inline styles
                       style={{ width: 194, height: 228 }}
                     />
                   </div>
@@ -44,28 +48,28 @@ function RoomTypePage({ numToShow }) {
                   <div className="product-deatil">
                     <h5 className="name">
                       <span style={{ fontSize: 16 }}>Loại phòng: </span>
-                      <a href="#">
+                      <Link to={`/room-type/${item.id}/`}>
                         {item.name} <span>{item.hotel_name}</span>
-                      </a>
+                      </Link>
                     </h5>
                     <p className="price-container">
                       <span>{item.price}</span>
                       <span className="text-muted">đ/ngày</span>
                     </p>
-                  </div>
-                  <div className="description">
-                    <p>{item.description} </p>
-                  </div>
-                  <div className="product-info smart-form">
-                    <div className="row">
-                      <div className="col-md-12 col-sm-12 col-xs-12 justify-content-center">
-                        <Link
-                          to="#"
-                          className="btn btn-success"
-                          onClick={() => handleRoomClick(item.id)}
-                        >
-                          Xem chi tiết
-                        </Link>
+                    <div className="description">
+                      <p>{item.description}</p>
+                    </div>
+                    <div className="product-info smart-form">
+                      <div className="row">
+                        <div className="col-md-12 col-sm-12 col-xs-12 justify-content-center">
+                          {/* Use Link component for button navigation */}
+                          <Link
+                            to={`/room-type/${item.id}/`}
+                            className="btn btn-success"
+                          >
+                            Xem chi tiết
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -78,4 +82,5 @@ function RoomTypePage({ numToShow }) {
     </>
   );
 }
+
 export default RoomTypePage;
